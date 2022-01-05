2ad6cba538779a90ace4871f16c0f879a1f7e0be
tehgunte <83183177+tehgunte@users.noreply.github.com>
2022-01-05 14:41:24 -0800

    JS and CSS addon

---
+++ js/js2.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{1:function(n,i,o){n.exports=o("g9n2")},g9n2:function(n,i){void 0===window.Finobe&&(window.Finobe={AppName:"",Currency:"",CurrencyImage:""}),void 0===window.Finobe.User&&(window.Finobe.User={UserId:0,UserName:null}),void 0===window.Finobe.Settings&&(window.Finobe.Settings={}),void 0===window.Finobe.Settings.Catalog&&(window.Finobe.Settings.Catalog={LoadPriceChart:!1,PriceData:""}),void 0===window.Finobe.Settings.Character&&(window.Finobe.Settings.Character={LoadScript:!1})}},[[1,0]]]);
+
+}
+/*
+     FILE ARCHIVED ON 18:22:44 Apr 20, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 13:57:53 Dec 03, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 123.826
+  exclusion.robots: 0.179
+  exclusion.robots.policy: 0.173
+  RedisCDXSource: 1.991
+  esindex: 0.009
+  LoadShardBlock: 97.723 (3)
+  PetaboxLoader3.datanode: 82.015 (4)
+  CDXLines.iter: 16.129 (3)
+  load_resource: 68.807
+  PetaboxLoader3.resolve: 39.089
+*/
