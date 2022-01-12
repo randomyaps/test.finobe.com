1b11ae220dfeed540bc58591e5c1310e1a262381
tehgunte <83183177+tehgunte@users.noreply.github.com>
2022-01-11 22:15:32 -0300

    fixed bug
    Thanks a lot to ktrain for testing!!!!

--- general/top.php
+++ general/top.php
@@ -9,9 +9,7 @@ echo "<title>". $ProjectName ."</title>
 <link rel='stylesheet' href='". $baseUrl ."/general/css/app.php'>
 <link rel='stylesheet' href='". $baseUrl ."/general/css/usetypekit.css'>
 <link rel='stylesheet' href='". $baseUrl ."/general/css/fontawesome-pro-v5.10.1/css/all.css'>
-<link rel='stylesheet' href='". $baseUrl ."/general/css/spacing.css'>
-<script src='". $baseUrl ."/general/js/sitejs.js' type='text/javascript'></script>
-<script src='". $baseUrl ."/general/js/sitejs2.js' type='text/javascript'></script>";
+<link rel='stylesheet' href='". $baseUrl ."/general/css/spacing.css'>";
 //embed!!!!
 echo "
 <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
