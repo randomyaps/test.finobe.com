2de7d78765311ac8719140ed6ae98de628a99287
tehgunte <83183177+tehgunte@users.noreply.github.com>
2022-01-12 12:20:00 -0300

    values file
    configurate values here!!!!
    that includes db too

---
+++ general/loadvls.php
@@ -0,0 +1,17 @@
+<?php
+//incase of any errors regarding HTTP access,
+//modify baseurl http to https.
+$baseUrl = "http://". $_SERVER['SERVER_NAME'] ."";
+$ProjectName = "Finobe";
+//THE NEXT FOLLOWING VARIABLES ARE EXAMPLES
+//I DO NOT CONDONE ANY ACTS DONE BY THE FOLLOWING TWITTER ACCOUNT OR DISCORD SERVER.
+//disclaimer was added because a tester found out that yourmom redirects to a 18+ server
+$DiscordJoin = "https://discord.gg/yourmom";
+$Twitter = "https://twitter.com/yourmom";
+//for easier modification, DB variables have been added, edit them here and you should be
+//all set-up!
+$hostdb = "localhost";
+$accdb = "root";
+$passdb = "yourmom";
+$namedb = "finobe";
+?>
