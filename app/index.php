514e2a40146f9fe7b95e82597fa3827f9aae7a91
tehgunte <83183177+tehgunte@users.noreply.github.com>
2022-01-13 12:29:29 -0300

    Delete index.php

--- app/index.php
+++
@@ -1,6 +0,0 @@
-<?php include($_SERVER['DOCUMENT_ROOT'] . '/general/loadvls.php'); ?>
-<?php
-	//oh shit our user is attempting to access a page that they are not supposed to see!!!
-	header('Location: '. $baseUrl .'/err.php?err=404');
-	die();
-?>
