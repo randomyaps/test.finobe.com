06b0af80f5f7e56f305ac9d17c7a1e90d0d951ca
tehstart <83183177+tehgunte@users.noreply.github.com>
2022-09-01 14:58:30 -0400

    Delete app directory

--- app/appscripting/GetError.php
+++
@@ -1,7 +0,0 @@
-<?php  if (count($errors) > 0) : ?>
-  	<?php foreach ($errors as $error) : ?>
-  	  <?php echo "
-		<div class='alert alert-danger text-center'>". $error ."</div>"; 
-		?>
-  	<?php endforeach ?>
-<?php  endif ?>
--- app/appscripting/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-	//oh shit our user is attempting to access a page that they are not supposed to see!!!
-	header('Location: '. $baseUrl .'/err.php?err=404');
-	die();
-?>
