303a4afedd3c93d81cccef4a2012b1f520d31df3
starterbee <83183177+starterbeeee@users.noreply.github.com>
2023-06-11 17:30:10 -0400

    Add files via upload

--- general/loadingValues/generalConfigs.php
+++ general/loadingValues/generalConfigs.php
@@ -5,6 +5,7 @@ $downloadUrl = $baseUrl ."/downloadexample";
 $CurrencyName = "Dius";
 $CurrencyIcon = "" . $baseUrl . "/imgs/diu_16.png";
 $allowedGameGenres = array('All', 'Featured', 'Original', 'Copies');
+$errorPages = array($baseUrl . '/err?err=404', $baseUrl . '/err?err=500', $baseUrl . '/err?err=401');
 $CurrPage = $_SERVER["REQUEST_URI"];
 
 $DiscordJoin = "https://-----/----";
@@ -12,7 +13,7 @@ $Twitter = "https://-----/----";
 
 $hostdb = "localhost";
 $accdb = "root";
-$passdb = "dwj]a";
+$passdb = "";
 $namedb = "finobe";
 
 $generaldb = new PDO("mysql:host=$hostdb;dbname=$namedb", $accdb, $passdb);
