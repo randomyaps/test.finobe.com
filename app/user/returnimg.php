5365bdf6ebb29a8f440bc4d82043be404b458245
starterbee <83183177+starterbeeee@users.noreply.github.com>
2023-06-17 15:20:29 -0400

    Delete returnimg.php

--- app/user/returnimg.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include($_SERVER['DOCUMENT_ROOT'] . '/general/loadingValues/generalConfigs.php'); 
-//code image retrieving and renders later
-//for now return THE LOADER
-echo(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/imgs/pending.gif'));
-?>
