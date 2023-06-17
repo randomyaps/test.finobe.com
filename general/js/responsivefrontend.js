1246f263582a35da57d05e3bf99d9aba720e1e40
starterbee <83183177+starterbeeee@users.noreply.github.com>
2023-06-17 09:47:58 -0400

    Group functions up
    Group functions up for easier. Js managing

--- general/js/responsivefrontend.js
+++ general/js/responsivefrontend.js
@@ -1,15 +1,18 @@
-function fetchPlaceList(string){
-	var games = $.ajax({
-		method:"GET",
-		url:"/general/jqueryResponsiveness/gamesGenre?genreType=" + string,
-		cache:false
-		}, 
-	);
-	games.done(function ( data ){
-		$('#gamesList').html(data);
-	});
-}
-
-function goPlace(id){
-	window.location="/places/view?id=" + id;
+var finobeApp = {
+	Games : {
+		fetchPlaceList : function fetchPlaceList(string){
+			var games = $.ajax({
+				method:"GET",
+				url:"/app/game/gamesGenre?genreType=" + string,
+				cache:false
+				}, 
+			);
+			games.done(function ( data ){
+				$('#gamesList').html(data);
+			});
+		},
+		goPlace : function goPlace(id){
+			window.location="/places/view?id=" + id;
+		}
+	}
 }
